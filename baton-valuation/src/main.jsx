5b19f4b4b25fc6e61621dabde9947372e6807bfe
sophieporitzky <sophie.poritzky@batonmarket.com>
2026-02-12 18:26:10 -0500

    Delete baton-valuation directory

--- baton-valuation/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import ConventionValuationApp from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ConventionValuationApp />
-  </React.StrictMode>,
-)
